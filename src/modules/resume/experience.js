93309bb1e5d6fe39d1921af4f644b0af00844219
pavan koka <pavan.koka@toppr.com>
2022-06-28 22:29:58 +0530

    changes

--- src/modules/resume/experience.js
+++ src/modules/resume/experience.js
@@ -1,42 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Zoom } from '@mui/material'
 import styles from './experience.module.scss'
 
+const experience = [
+    {
+        title: 'Toppr Pvt Ltd',
+        role: 'Senior Software Engineer',
+        date: 'Aug 2021 - Present',
+        description: [
+            'I was re‐hired by Toppr due to my past work experience over there by offering a Senior Software Engineer Role.',
+            "I'm currently building a website from scratch using NextJS which merges top features of byjus and toppr into one Application.",
+            "I've dealt with Authentication, Redirections, Login flow, Repo setup, Styling options, Redux, Reducers, Sagas, Static variables, Helpers, server file, interceptors, proxy configuration for the Byjus Future School Web Application.",
+            "I've worked on Webview for Byju's future school app which squashes top features of byjus and toppr under one roof using Swig Templates and MathJax for both IOS and Android.",
+        ],
+    },
+    {
+        title: 'Innovaccer',
+        role: 'SDE - I',
+        date: 'Dec 2020 - Aug 2021',
+        description: [
+            "I've developed few pages on client side using React, Redux and Thunks.",
+            "I've learned to use Typescript and implemented in all the repositories.",
+            "Mostly I've dealt with bugs, small features, add-ons to the stable version of the builds.",
+        ],
+    },
+    {
+        title: 'Toppr Pvt Ltd',
+        role: 'Software Engineer',
+        date: 'Jun 2007 - Mar 2012',
+        description: [
+            "I've developed real-time communication between tutor and student using sockets which is called as toppr-doubts.",
+            "I've worked on all webview screens of Toppr-Ask using Swig Templates and MathJax to render the html wrapped content.",
+            'Toppr-Ask is an application that attracts traffic and using server side rending for effective googlebot crawling.',
+            "I've worked in developing multiple pages, search results page, question upload ... etc.",
+        ],
+    },
+]
+
 function Experience() {
-  return (
-    <div id='experience' className={styles.wrapper}>
-        <div className={styles.logo}>Experience</div>
-        <div className={styles.content}>
-            <p className={styles.title}>Toppr Pvt Ltd</p>
-            <p className={styles.role}>SENIOR SOFTWARE ENGINEER</p>
-            <p className={styles.date}>Aug 2021 - Present</p>
-            <div className={styles.description}>
-                <p>I was re‐hired by Toppr due to my past work experience over there by offering a Senior Software Engineer Role.</p>
-                <p>I'm currently building a website from scratch using NextJS which merges top features of byjus and toppr into one Application.</p>
-                <p>I've dealt with Authentication, Redirections, Login flow, Repo setup, Styling options, Redux, Reducers, Sagas, Static variables, Helpers, server file, interceptors, proxy configuration for the Byjus Future School Web Application.</p>
-                <p>I've worked on Webview for Byju's future school app which squashes top features of byjus and toppr under one roof using Swig Templates and MathJax for both IOS and Android.</p>
-            </div>
-            <div style={{margin: '24px 0'}}>
-                <p className={styles.title}>Innovaccer</p>
-                <p className={styles.role}>SDE - I</p>
-                <p className={styles.date}>Dec 2020 - Aug 2021</p>
-                <div className={styles.description}>
-                    <p>I've developed few pages on client side using React, Redux and Thunks.</p>
-                    <p>I've learned to use Typescript and implemented in all the repositories.</p>
-                    <p>Mostly I've dealt with bugs, small features, add-ons to the stable version of the builds.</p>
-                </div>
-            </div>
-            <p className={styles.title}>Toppr Pvt Ltd</p>
-            <p className={styles.role}>Software Engineer</p>
-            <p className={styles.date}>Jun 2007 - Mar 2012</p>
-            <div className={styles.description}>
-                <p>I've developed real-time communication between tutor and student using sockets which is called as toppr-doubts.</p>
-                <p>I've worked on all webview screens of Toppr-Ask using Swig Templates and MathJax to render the html wrapped content.</p>
-                <p>Toppr-Ask is an application that attracts traffic and using server side rending for effective googlebot crawling.</p>
-                <p>I've worked in developing multiple pages, search results page, question upload ... etc.</p>
+    const [selected, setSelected] = useState(-1)
+
+    return (
+        <div id="experience" className={styles.wrapper}>
+            <div className={styles.logo}>Experience</div>
+            <div className={styles.content}>
+                {experience.map((card, id) => (
+                    <div
+                        className={styles.item}
+                        key={`experince-card-${id}`}
+                        onMouseEnter={() => setSelected(id)}
+                        onMouseLeave={() => setSelected(-1)}
+                    >
+                        {card.role}
+                        <Zoom in={selected === id}>
+                            <div className={styles.card}>
+                                <p className={styles.title}>{card.title}</p>
+                                <p className={styles.role}>{card.role}</p>
+                                <p className={styles.date}>{card.date}</p>
+                                <div className={styles.description}>
+                                    {card.description.map((point) => (
+                                        <p>{point}</p>
+                                    ))}
+                                </div>
+                            </div>
+                        </Zoom>
+                    </div>
+                ))}
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Experience
--- src/utils/stackToModal.js
+++ src/utils/stackToModal.js
@@ -20,49 +20,52 @@ function Animation() {
     return (
         <AnimateSharedLayout type="crossfade">
             <div className={styles.cardsWrapper}>
-                {educationDetails.map((item, index) => (
-                    <motion.div
-                        id={item.id}
-                        layoutId={item.id}
-                        className={cx(styles.card, selectedId === index ? styles.selected : null)}
-                        whileHover={{
-                            scale: 1.04,
-                            x: 20,
-                        }}
-                        style={{}}
-                        animate={{
-                            // x: selectedId === index  ? 100 : 0,
-                            // y: selectedId === index ? ((1 - selectedId) * 100) : 0,
-                        }}
-                        transition={{
-                            duration: 0.25,
-                        }}
-                        onClick={() => handleLayoutClick(index)}
-                        // onMouseEnter={() => setSelectedId(index)}
-                        // onMouseLeave={() => setSelectedId(-1)}
-                    >
-                        {selectedId === index ? (
-                            <motion.div 
-                                className={styles.cardModal}
-                                initial={{
-                                    scale: 0,
-                                }}
-                                animate={{
-                                    scale: 1,
-                                }}
-                                onClick={e => e.stopPropagation()}
-                            >
-                                <img src={CROSS} className={styles.close} alt='cross' onClick={() => setSelectedId(-1)} />
-                                <p className={styles.title}>{item.college}</p>
-                                <p className={styles.date}>{item.date}</p>
-                                <div className={styles.description}>
-                                    {item.desc.map(desc => <p>{desc}</p>)}
-                                </div>
-                            </motion.div>
-                        ) : <p className={styles.title}>{item.id}</p>}
-                        
-                    </motion.div>
-                ))}
+                <AnimatePresence>
+                    {educationDetails.map((item, index) => (
+                        <motion.div
+                            id={item.id}
+                            layoutId={item.id}
+                            className={cx(styles.card, selectedId === index ? styles.selected : null)}
+                            whileHover={{
+                                scale: 1.04,
+                                x: 20,
+                            }}
+                            style={{}}
+                            animate={{
+                                // x: selectedId === index  ? 100 : 0,
+                                // y: selectedId === index ? ((1 - selectedId) * 100) : 0,
+                            }}
+                            transition={{
+                                duration: 0.25,
+                            }}
+                            onClick={() => handleLayoutClick(index)}
+                            // onMouseEnter={() => setSelectedId(index)}
+                            // onMouseLeave={() => setSelectedId(-1)}
+                        >
+                            {selectedId === index ? (
+                                <motion.div 
+                                    className={styles.cardModal}
+                                    initial={{
+                                        scale: 0,
+                                    }}
+                                    animate={{
+                                        scale: 1,
+                                    }}
+                                    onClick={e => e.stopPropagation()}
+                                >
+                                    <img src={CROSS} className={styles.close} alt='cross' onClick={() => setSelectedId(-1)} />
+                                    <p className={styles.title}>{item.college}</p>
+                                    <p className={styles.date}>{item.date}</p>
+                                    <div className={styles.description}>
+                                        {item.desc.map(desc => <p>{desc}</p>)}
+                                    </div>
+                                </motion.div>
+                            ) : <p className={styles.title}>{item.id}</p>}
+                            
+                        </motion.div>
+                    ))}
+                </AnimatePresence>
+                
             </div>
         </AnimateSharedLayout>
   )
